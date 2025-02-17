b405ff0ae80e20be2ae27261a59b0bccc00adede
laktosterror <55037659+laktosterror@users.noreply.github.com>
2025-02-17 11:02:28 +0100

    added users list and mapget for users and users/id

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
+using MinimalAPI_par;
+
+List<User> users =
+[
+    new User { Id = 1, Name = "Dave" },
+    new User { Id = 2, Name = "Tony" },
+    new User { Id = 3, Name = "Janne" }
+];
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/users", () => users);
+
+app.MapGet("/users/{id}", (int id) =>
+{
+    var user = users.FirstOrDefault(u => u.Id == id);
+    return user is not null ? Results.Ok(user) : Results.NotFound($"User not found with ID: {id}");
+});
 
 app.Run();
