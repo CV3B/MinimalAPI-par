8c76a4cef8f1780f48c3f98a23e50bc09e538aba
laktosterror <55037659+laktosterror@users.noreply.github.com>
2025-02-17 11:21:07 +0100

    implemented users route with all crud operations

--- Program.cs
+++ Program.cs
@@ -2,22 +2,71 @@ using MinimalAPI_par;
 
 List<User> users =
 [
-    new User { Id = 1, Name = "Dave" },
-    new User { Id = 2, Name = "Tony" },
-    new User { Id = 3, Name = "Janne" }
+    new() { Id = 1, Name = "Dave" },
+    new() { Id = 2, Name = "Tony" },
+    new() { Id = 3, Name = "Janne" }
 ];
 
 var builder = WebApplication.CreateBuilder(args);
+ 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "Minimal API";
+    config.Title = "MinimalAPI v1";
+    config.Version = "v1";
+});
+ 
 var app = builder.Build();
+ 
+// Configure the HTTP request pipeline.
+if(app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "Minimal Exercise API";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
 
-app.MapGet("/", () => "Hello World!");
-
+//GET
 app.MapGet("/users", () => users);
 
 app.MapGet("/users/{id}", (int id) =>
 {
     var user = users.FirstOrDefault(u => u.Id == id);
     return user is not null ? Results.Ok(user) : Results.NotFound($"User not found with ID: {id}");
+});
+
+//POST
+app.MapPost("/users", (User user) =>
+{
+    if (users.Any(u => u.Id == user.Id))
+        return Results.BadRequest($"User with ID: {user.Id} already exists");
+    users.Add(user);
+    return Results.Created($"/users/{user.Id}", user);
+});
+
+//PUT
+app.MapPut("/users/{id}", (int id, User updatedUser) =>
+{
+    var user = users.FirstOrDefault(u => u.Id == id);
+    if (user is null) return Results.NotFound($"User not found with ID: {id}");
+    
+    user.Name = updatedUser.Name;
+    return Results.Ok(user);
+});
+
+//DELETE
+app.MapDelete("/users/{id}", (int id) => 
+{
+    var user = users.FirstOrDefault(u => u.Id == id);
+    if (user is null) return Results.NotFound($"User not found with ID: {id}");
+    users.Remove(user);
+    return Results.Ok($"User with id {id} deleted");
 });
 
 app.Run();
